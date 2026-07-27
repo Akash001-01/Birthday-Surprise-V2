e8a79d4870ab92743e99929ccc8a03063193a957
Akash001-01 <sakash.akashs8@gmail.com>
2026-07-27 13:23:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,66 @@
+const steps = [
 
+document.getElementById("step1"),
+
+document.getElementById("step2"),
+
+document.getElementById("step3"),
+
+document.getElementById("step4"),
+
+document.getElementById("step5")
+
+];
+
+let currentStep = 0;
+
+function showStep(index){
+
+steps.forEach(step=>{
+
+step.classList.add("hidden");
+
+});
+
+steps[index].classList.remove("hidden");
+
+}
+
+setTimeout(()=>{
+
+showStep(1);
+
+},2500);
+
+setTimeout(()=>{
+
+showStep(2);
+
+},6500);
+
+setTimeout(()=>{
+
+showStep(3);
+
+},12000);
+
+setTimeout(()=>{
+
+showStep(4);
+
+},17500);
+
+document.getElementById("beginBtn").addEventListener("click",()=>{
+
+document.body.style.transition="1.5s";
+
+document.body.style.opacity="0";
+
+setTimeout(()=>{
+
+// Next Section (Envelope)
+alert("💌 Envelope Coming in Section 2");
+
+},1500);
+
+});
